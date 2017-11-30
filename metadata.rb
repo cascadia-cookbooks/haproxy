8be960508d29f3362b7c235649a90460723f2e92
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-29 19:40:51 -0800

    0.1.0

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'cas_haproxy'
+maintainer 'Cascadia Cookbooks'
+maintainer_email 'null'
+license 'GPL-3.0'
+description 'Manges HAProxy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+source_url 'https://github.com/cascadia-cookbooks/haproxy'
+issues_url 'https://github.com/cascadia-cookbooks/haproxy/issues'
+
+supports 'ubuntu', '>= 16.04'
+supports 'redhat', '>= 7.0'
+supports 'centos', '>= 7.0'
+supports 'debian', '>= 8.0'
