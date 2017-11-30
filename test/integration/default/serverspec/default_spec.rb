43f3c6b63979eadff482f70bf1eba083b734bd3c
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-30 11:11:14 -0800

    Test skeleton for templates

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,4 +4,44 @@ describe 'cas_haproxy::default' do
     describe package('haproxy') do
         it { should be_installed }
     end
+
+    describe file('/etc/haproxy/haproxy.cfg') do
+        it { should exist }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should include '' }
+    end
+
+    describe file('/etc/default/haproxy') do
+        it { should exist }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should include '' }
+    end
+
+    describe file('/lib/systemd/system/haproxy.service') do
+        it { should exist }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should include '' }
+    end
+
+    describe file('/etc/haproxy/sites/one.example.com.cfg') do
+        it { should exist }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should include '' }
+    end
+
+    describe file('/etc/haproxy/sites/two.example.com.cfg') do
+        it { should exist }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+        it { should be_mode 644 }
+        its(:content) { should include '' }
+    end
 end
