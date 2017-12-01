b41d7ea7037999b16fa6fa032dfe5b0e5d678389
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-12-01 14:21:08 -0800

    Test for sites cfg content

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -29,13 +29,18 @@ describe 'cas_haproxy::default' do
         it { should be_mode 644 }
         its(:content) { should include '' }
     end
+end
 
+describe 'cas_haproxy::sites' do
     describe file('/etc/haproxy/sites/blog.cfg') do
         it { should exist }
         it { should be_owned_by 'root' }
         it { should be_grouped_into 'root' }
         it { should be_mode 644 }
-        its(:content) { should include '' }
+        its(:content) { should include 'frontend blog' }
+        its(:content) { should include 'bind *:6061' }
+        its(:content) { should include 'acl host_blog hdr(host) -i example.com blog.example.com' }
+        its(:content) { should include 'use_backend blog_cluster if host_blog' }
     end
 
     describe file('/etc/haproxy/sites/wiki.cfg') do
@@ -43,6 +48,9 @@ describe 'cas_haproxy::default' do
         it { should be_owned_by 'root' }
         it { should be_grouped_into 'root' }
         it { should be_mode 644 }
-        its(:content) { should include '' }
+        its(:content) { should include 'frontend wiki' }
+        its(:content) { should include 'bind *:80' }
+        its(:content) { should include 'acl host_wiki hdr(host) -i wiki.example.com' }
+        its(:content) { should include 'use_backend wiki_cluster if host_wiki' }
     end
 end
