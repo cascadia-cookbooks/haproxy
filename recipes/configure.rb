e70ed1e8f2ddf2e7337f6cd1149b8c0d743792ab
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-12-02 15:48:50 -0800

    Template configuraton

---
+++ recipes/configure.rb
@@ -0,0 +1,56 @@
+#
+# Cookbook:: cas_haproxy
+# Recipe:: configure
+# Description:: Configure HAProxy
+#
+
+template "HAProxy defaults file" do
+    source   'haproxy.erb'
+    path     '/etc/default/haproxy'
+    owner    'root'
+    group    'root'
+    mode     0644
+    backup   false
+    action   :create
+end
+
+template "HAProxy service configuration" do
+    source   'haproxy.service.erb'
+    path     '/lib/systemd/system/haproxy.service'
+    owner    'root'
+    group    'root'
+    mode     0644
+    backup   false
+    action   :create
+end
+
+template "Default HAProxy configuration file" do
+    source   'haproxy.cfg.erb'
+    path     '/etc/haproxy/haproxy.cfg'
+    owner    'root'
+    group    'root'
+    mode     0644
+    backup   false
+    action   :create
+end
+
+directory '/etc/haproxy/sites' do
+    owner  'root'
+    group  'root'
+    mode   '0755'
+    action :create
+end
+
+node['cas_haproxy']['sites'].each do |site|
+    template "Configuration for #{site['id']}" do
+        source   'site.cfg.erb'
+        path     "/etc/haproxy/sites/#{site['id']}.cfg"
+        owner    'root'
+        group    'root'
+        mode     0644
+        backup   false
+        action   :create
+
+        variables(site)
+    end
+end
