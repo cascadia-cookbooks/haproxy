61d31c5b9d75064938dd312954fd6d7d23b45249
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-12-06 17:39:35 -0800

    Rework tests for stats

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,9 +10,6 @@ describe 'cas_haproxy::default' do
         it { should be_owned_by 'root' }
         it { should be_grouped_into 'root' }
         it { should be_mode 644 }
-        its(:content) { should include 'stats socket /var/run/haproxy.sock user root group root mode 600 level operator' }
-        its(:content) { should include 'stats timeout 10s' }
-        its(:content) { should include 'stats maxconn 10' }
     end
 
     describe file('/etc/default/haproxy') do
@@ -63,8 +60,24 @@ describe 'cas_haproxy::sites' do
         its(:content) { should include 'backend wiki_cluster' }
         its(:content) { should include 'balance roundrobin' }
         its(:content) { should include 'mode tcp' }
-        its(:content) { should include 'server web4.local web4.local:80 check' }
-        its(:content) { should include 'server web5.local web5.local:80 check' }
+        its(:content) { should include 'server web4.local web4.local check' }
+        its(:content) { should include 'server web5.local web5.local check' }
         its(:content) { should include 'server web6.local web6.local:5000 check' }
+    end
+end
+
+describe 'cas_haproxy::stats' do
+    describe file('/etc/haproxy/haproxy.cfg') do
+        its(:content) { should include 'stats socket /var/run/haproxy.sock user root group root mode 600 level operator' }
+        its(:content) { should include 'stats timeout 10s' }
+        its(:content) { should include 'stats maxconn 10' }
+        its(:content) { should include 'listen stats' }
+        its(:content) { should include 'stats enable' }
+        its(:content) { should include 'mode http' }
+        its(:content) { should include 'bind :9001' }
+        its(:content) { should include 'stats realm HAProxy-Stats' }
+        its(:content) { should include 'stats uri /haproxy_stats' }
+        its(:content) { should include 'stats auth username:password' }
+        its(:content) { should include 'stats hide-version' }
     end
 end
