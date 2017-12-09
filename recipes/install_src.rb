096f07b93f91840462c63cab00c30c7ce3f643fe
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-12-08 17:12:47 -0800

    Break out HAProxy version

--- recipes/install_src.rb
+++ recipes/install_src.rb
@@ -3,3 +3,6 @@
 # Recipe:: install_src
 # Description:: Install HAProxy from source
 #
+
+# Break up HAProxy version
+version_major, version_minor, version_patch = node['cas_haproxy']['version'].split('.')
