74848c0c7e0b2a627ae2a1c69ebd8b0327c852e6
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-12-08 17:22:52 -0800

    Fetch source tarball

--- recipes/install_src.rb
+++ recipes/install_src.rb
@@ -7,3 +7,12 @@
 # Break up HAProxy version
 version = node['cas_haproxy']['version']
 version_major, version_minor, version_patch = version.split('.')
+
+# Build source URL
+source_url = "https://www.haproxy.org/download/#{version_major}.#{version_minor}/src/haproxy-#{version}.tar.gz"
+
+# Fetch source tarball 
+remote_file 'HAProxy source tarball' do
+    source source_url
+    path   "/tmp/haproxy-#{version}.tar.gz"
+end
